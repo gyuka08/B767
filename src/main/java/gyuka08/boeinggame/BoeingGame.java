4b50b7f64a13106f12f3e1ce4a6bde485898df85
squareVanilla <kangstaridea@proton.me>
2024-09-05 22:43:28 +0900

    ArabFixed

--- src/main/java/gyuka08/boeinggame/BoeingGame.java
+++ src/main/java/gyuka08/boeinggame/BoeingGame.java
@@ -1,2 +1,23 @@
-package main.java.gyuka08.boeinggame;public class BoeingGame {
+package main.java.gyuka08.boeinggame;
+
+import javax.swing.*;
+
+public class BoeingGame {
+    public static void main(String[] Akbar) throws Exception {
+        int windowWidth = 360;
+        int windowHeight = 640;
+
+        JFrame window = new JFrame("Jihad");
+//        window.setVisible(true);
+        window.setSize(windowWidth, windowHeight);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Canvas windowCanvas = new Canvas();
+        window.add(windowCanvas);
+        window.pack();
+        window.setVisible(true);
+
+    }
 }
--- src/main/java/gyuka08/boeinggame/Canvas.java
+++ src/main/java/gyuka08/boeinggame/Canvas.java
@@ -1,2 +1,37 @@
-package main.java.gyuka08.boeinggame;public class Canvas {
+package main.java.gyuka08.boeinggame;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Canvas extends JPanel{
+    int windowWidth = 360;
+    int windowHeight = 640;
+
+    //Image
+    Image newYorkImage;
+    Image boeingImage;
+    Image topBuildingImage;
+    Image buildingImage;
+
+
+    Canvas() {
+        setPreferredSize(new Dimension(windowWidth, windowHeight));
+        setBackground(Color.blue);
+
+        //image loaded
+        newYorkImage = new ImageIcon(getClass().getResource("src/main/resources/assets/Image/spr_newyork.png")).getImage();
+        boeingImage = new ImageIcon(getClass().getResource("src/main/resources/assets/Image/spr_boeing767.png")).getImage();
+        topBuildingImage = new ImageIcon(getClass().getResource("src/main/resources/assets/Image/spr_building")).getImage();
+        buildingImage = new ImageIcon(getClass().getResource("src/main/resources/assets/Image/spr_building")).getImage();
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        draw(graphics);
+    }
+
+    public void draw(Graphics graphics) {
+        //newyok
+        graphics.drawImage(newYorkImage, 0, 0, windowWidth, windowHeight, null);
+    }
 }
