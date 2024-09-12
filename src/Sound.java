c7c685b3bf369a17d76adccca6e9432aa551eaee
Acord <kangtheseed@gmail.com>
2024-09-12 21:28:20 +0900

    Sound In Progress

---
+++ src/Sound.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class Sound {
+    String bgm1 = "main/resources/assets/Sound/snd_al_quovlu_sawarim.wav";
+
+
+    public static void playSound(String fileLocation) {
+
+    }
+}
